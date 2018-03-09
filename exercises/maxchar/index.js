d5960f9391061f898482923404297725ddfb3a73
Dustin Doan <dustin.ddoan@gmail.com>
2018-03-08 20:40:20 -0800

    find max char in str

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {};
+    let max = 0;
+    let maxChar = ''
+
+    for (let char of str) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    for (let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+
+    console.log(charMap)
+    console.log(maxChar)
+    return maxChar
+}
 
 module.exports = maxChar;
